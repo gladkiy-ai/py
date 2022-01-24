02103beb8261170357b46608a4a0b94380e0a669
GladkayaIF <axatasha@gmail.com>
2022-01-24 20:51:20 +0300

    modify nodes data

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from pprint import pprint
+""" ****** Стандартный алгоритм обхода графа в глубину. ****
+1. Начните с размещения любой вершины графа на вершине стека.
+2. Возьмите верхний элемент стека и добавьте его в список посещенных.
+3. Создайте список смежных узлов этой вершины. Добавьте те, 
+ которых нет в списке посещенных, в начало стека.
+4. Продолжайте повторять шаги 2 и 3, пока стек не станет пустым.
+"""
+def dfs(graph, start, visited=[]):
+    if start not in visited:
+        visited.append(start)
+        for neighbour in graph[start]:
+            if neighbour in graph.keys():
+                dfs(graph, neighbour, visited)
 
+def dfs_max_deep(graph, start, visited=[]):
+    d = 1
+    if start not in visited:
+        visited.append(start)
+        for neighbor in graph[start]:
+            if neighbour in graph.keys():
+                d = max(d, dfs_max_deep(graph, neighbor, visited) + 1)
+    return d
 
 def concatGraph(graph_1, graph_2):
     graph_out ={}
@@ -64,41 +85,41 @@ if __name__ == '__main__':
     a03 = ('A03', 3)
     a04 = ('A04', 4)
     a05 = ('A05', 5)
-    a06 = ('A06', 16)
-    a07 = ('A07', 17)
-    a08 = ('A08', 18)
-    a09 = ('A09', 98)
-    a10 = ('A10', 99)
-    b01 = ('B01', 10)
-    b02 = ('B02', 11)
-    b03 = ('B03', 12)
-    b04 = ('B04', 13)
-    b05 = ('B05', 25)
-    b06 = ('B06', 26)
-    b07 = ('B07', 27)
-    b08 = ('B08', 28)
-    b09 = ('B09', 29)
-    b10 = ('B10', 30)
+    a16 = ('A16', 16)
+    a17 = ('A17', 17)
+    a18 = ('A18', 18)
+    a98 = ('A98', 98)
+    a99 = ('A99', 99)
+    b10 = ('B10', 10)
+    b11 = ('B11', 11)
+    b12 = ('B12', 12)
+    b13 = ('B13', 13)
+    b25 = ('B25', 25)
+    b26 = ('B26', 26)
+    b27 = ('B27', 27)
+    b28 = ('B28', 28)
+    b29 = ('B29', 29)
+    b30 = ('B30', 30)
 
     graph1 = {a01: a02,
              a02: a03,
              a03: a04,
              a04: a05,
-             a05: a06,
-             a06: a07,
-             a07: a08,
-             a08: a09,
-             a09: a10
+             a05: a16,
+             a16: a17,
+             a17: a18,
+             a18: a98,
+             a98: a99
               }
-
-    graph2 = {b01: b02,
-             b02: b03,
-             b04: b05,
-             b05: b06,
-             b06: b07,
-             b07: b08,
-             b08: b09,
-             b09: b10
+    graph2 = {b10: b11,
+             b11: b12,
+             b12: b13,
+             b13: b25,
+             b25: b26,
+             b26: b27,
+             b27: b28,
+             b28: b29,
+             b29: b30
               }
     print('graph1--------------------------')
     pprint(graph1)
@@ -108,7 +129,9 @@ if __name__ == '__main__':
     isCyclic(graph2)
     print('isOrder graph2---------------------------')
     isOrder(graph2)
-    # print('Concat graph1 + graph 2---------------------------')
+    print('Concat graph1 + graph 2---------------------------')
+    print(dfs(graph2,b10))
+    print(dfs_max_deep(graph2,b10))
     # graph3 = concatGraph(graph1, graph2)
     # pprint(graph3)
     # isOrder(graph3)
